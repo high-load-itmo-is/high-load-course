cec1d99561502a8537f61aca0ae93b70d746fcd0
Artyom Moiseenko <aymoiseenko3@gmail.com>
2025-11-28 00:09:06 +0300

    x2 bucket size

--- src/main/kotlin/ru/quipy/apigateway/APIController.kt
+++ src/main/kotlin/ru/quipy/apigateway/APIController.kt
@@ -76,7 +76,7 @@ class APIController {
     private val rateLimiter = LeakingBucketRateLimiter(
         11,
         Duration.ofSeconds(1),
-        100
+        200
     )
 
     @PostMapping("/orders/{orderId}/payment")

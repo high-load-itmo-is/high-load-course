ab9dd9681fda6b9e40e551c44f95af376088dafc
Artyom Moiseenko <aymoiseenko3@gmail.com>
2025-11-27 23:41:46 +0300

    binary search step 3

--- src/main/kotlin/ru/quipy/apigateway/APIController.kt
+++ src/main/kotlin/ru/quipy/apigateway/APIController.kt
@@ -76,7 +76,7 @@ class APIController {
     private val rateLimiter = LeakingBucketRateLimiter(
         11,
         Duration.ofSeconds(1),
-        60
+        90
     )
 
     @PostMapping("/orders/{orderId}/payment")

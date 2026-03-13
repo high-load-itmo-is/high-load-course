3f9f7ae761519dd36391ddc6affdb7b518137ce4
OHtuzh <aymoiseenko3@gmail.com>
2026-03-13 12:10:16 +0300

    wait jobs & inc max connections 100 -> 200

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -36,7 +36,7 @@ class PaymentExternalSystemAdapterImpl(
         val mapper = ObjectMapper().registerKotlinModule()
 
         val connectionProvider: ConnectionProvider = ConnectionProvider.builder("payment-provider")
-            .maxConnections(100)
+            .maxConnections(200)
             .pendingAcquireTimeout(Duration.ofSeconds(120))
             .maxIdleTime(Duration.ofSeconds(60))
             .build()

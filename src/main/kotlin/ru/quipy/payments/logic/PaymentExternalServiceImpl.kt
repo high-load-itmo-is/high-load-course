836f742fa554e3e605515eba424c79c53380f4a3
Artyom Moiseenko <aymoiseenko3@gmail.com>
2026-03-19 01:12:37 +0300

    dispatch 5xx

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -2,15 +2,19 @@ package ru.quipy.payments.logic
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.github.resilience4j.circuitbreaker.CircuitBreaker
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry
+import io.micrometer.core.instrument.MeterRegistry
+import io.netty.channel.ChannelOption
 import kotlinx.coroutines.*
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.sync.Semaphore
+import okhttp3.internal.wait
 import org.slf4j.LoggerFactory
-import io.micrometer.core.instrument.MeterRegistry
-import io.netty.channel.ChannelOption
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.WebClientResponseException.TooManyRequests
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.bodyToMono
 import reactor.netty.http.HttpProtocol
 import reactor.netty.http.client.HttpClient
@@ -18,14 +22,13 @@ import reactor.netty.resources.ConnectionProvider
 import reactor.netty.resources.LoopResources
 import ru.quipy.common.utils.NamedThreadFactory
 import ru.quipy.common.utils.SlidingWindowRateLimiter
-import ru.quipy.core.EventSourcingService
-import ru.quipy.payments.api.PaymentAggregate
-import ru.quipy.payments.logic.OrderPayer.Companion
 import java.net.SocketTimeoutException
 import java.time.Duration
 import java.util.*
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
+import kotlin.time.DurationUnit
 
 
 class PaymentExternalSystemAdapterImpl(
@@ -59,6 +62,16 @@ class PaymentExternalSystemAdapterImpl(
         ).asCoroutineDispatcher()
 
         val paymentScope = CoroutineScope(SupervisorJob() + sharedDispatcher)
+
+        var circuitBreakerConfig: CircuitBreakerConfig = CircuitBreakerConfig.custom()
+            .slidingWindowSize(100)
+            .failureRateThreshold(10F)
+            .waitDurationInOpenState(Duration.ofSeconds(60))
+            .build()
+        var circuitBreakerRegistry: CircuitBreakerRegistry = CircuitBreakerRegistry.of(circuitBreakerConfig)
+        var circuitBreaker: CircuitBreaker = circuitBreakerRegistry
+            .circuitBreaker("cb", circuitBreakerConfig)
+
     }
 
     private val serviceName = properties.serviceName
@@ -109,9 +122,14 @@ class PaymentExternalSystemAdapterImpl(
     }
 
     private suspend fun executePaymentReactive(paymentId: UUID, amount: Int, transactionId: UUID) {
+        while (!circuitBreaker.tryAcquirePermission()) {
+            delay(1000)
+        }
+        val start = now()
         try {
             semaphore.acquire()
             rateLimiter.tickSuspending()
+
             val responseBody = webClient.post()
                 .uri { uriBuilder ->
                     uriBuilder.path("/external/process")
@@ -128,19 +146,22 @@ class PaymentExternalSystemAdapterImpl(
                 .doFinally {
                     semaphore.release()
                 }
+                .timeout(Duration.ofSeconds(1))
                 .awaitSingle()
+            val end = now()
 
             successCounter.increment()
-        } catch (e: TooManyRequests) {
-            logger.error("[$accountName] Payment failed for txId: $transactionId, with 429, retrying")
-            executePaymentReactive(paymentId, amount, transactionId)
+            circuitBreaker.onSuccess(end - start, TimeUnit.MILLISECONDS)
         } catch (e: Exception) {
             logger.error("[$accountName] Payment failed for txId: $transactionId, payment: $paymentId", e)
 
             when (e) {
+                is WebClientResponseException.TooManyRequests -> executePaymentReactive(paymentId, amount, transactionId)
                 is TimeoutException, is SocketTimeoutException -> timeoutCounter.increment()
                 else -> errorCounter.increment()
             }
+            val end = now()
+            circuitBreaker.onError(end - start, TimeUnit.MILLISECONDS, e)
         }
     }
 
